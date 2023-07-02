2092a2a5f00b925cdd6872934ffabce1343032e6
Duy Anh <ndanhdev@gmail.com>
2023-07-03 06:01:26 +0700

    #12

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,15 @@ import Footer from '../components/footer/FooterDefault';
 import styles from '../styles/Home.module.scss';
 import { Inter } from 'next/font/google';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+    const router = useRouter();
+    const userid = 23;
+    const username = 'karma';
+
     return (
         <>
             <h1>Hello</h1>
@@ -16,6 +21,30 @@ export default function Home() {
             <Link href="/users">Index/Users</Link>
             <br />
             <Link href="/services">Services</Link>
+            <br />
+            <button
+                onClick={() => {
+                    router.push('/users');
+                }}
+            >
+                Navigate to users
+            </button>
+            <br />
+            <button
+                onClick={() => {
+                    router.push('/services');
+                }}
+            >
+                Navigate to services
+            </button>
+            <br />
+            <button
+                onClick={() => {
+                    router.push(`/users/${userid}/${username}`);
+                }}
+            >
+                Navigate to user id 5 name is karma
+            </button>
         </>
     );
 }
--- pages/users/index.js
+++ pages/users/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import styles from '../../styles/Home.module.scss';
 import Link from 'next/link';
-export default function users() {
+import { useRouter } from 'next/router';
+export default function Index() {
+    const router = useRouter();
+
     return (
         <div className={styles.container}>
             <h1>user</h1>
